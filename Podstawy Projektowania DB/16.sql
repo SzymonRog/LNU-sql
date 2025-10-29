99c864b84fc80e0f64ee061ce02276c154e41638
SzymonRog <rogalaszymon78@gmail.com>
2025-10-29 18:54:07 +0100

    Add tags to show the order

--- Podstawy Projektowania DB/16.sql
+++ Podstawy Projektowania DB/16.sql
@@ -1,3 +1,4 @@
+-- 1.Copy --
 CREATE TABLE clients (
     client_id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
@@ -7,13 +8,16 @@ CREATE TABLE clients (
     place TEXT NOT NULL
 );
 
+-- 2.Copy --
 INSERT INTO clients (first_name, family_name, street, zip, place)
 SELECT DISTINCT first_name, family_name, street, zip, place
 FROM halva_sales
 ORDER BY ROWID;
 
+-- 3.Copy --
 create table halva(halva_id integer primary key, [type] text not null, price float not null)
 
+-- 4.Copy --
 INSERT INTO halva (type, price)
 SELECT DISTINCT hs.type, hs.price
 FROM halva_sales AS hs
@@ -29,8 +33,14 @@ ORDER BY CASE hs.type
     WHEN 'Greek' THEN 3
     ELSE 4 END;
 
-
+-- 5.Copy --
 CREATE TABLE halva_sales_new (order_id INTEGER PRIMARY KEY AUTOINCREMENT, client_id INTEGER NOT NULL, halva_id INTEGER NOT NULL, amount FLOAT NOT NULL, price FLOAT NOT NULL, value FLOAT NOT NULL, [date] TEXT NOT NULL, FOREIGN KEY (client_id) REFERENCES clients(client_id), FOREIGN KEY (halva_id) REFERENCES halva(halva_id));
+
+-- 6.Copy --
 insert into  halva_sales_new(client_id,halva_id,amount,price,value,[date]) select client_id,halva_id,amount,halva_sales.price,value,[date] from halva_sales join clients on  halva_sales.first_name = clients.first_name AND halva_sales.family_name = clients.family_name join halva on halva_sales.type = halva.type
+
+-- 7.Copy --
 drop table halva_sales
+
+-- 8.Copy --
 alter table halva_sales_new rename to halva_sales
